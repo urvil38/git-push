c281ffee013a87ecc5b71f5154d507973e3619d2
Urvil Patel <patelurvil38@gmail.com>
2018-01-22 09:26:32 +0530

    added logo package

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"github.com/git-cli"
+	"github.com/git-cli/logo"
+	"fmt"
 )
 
+var gitlogo = `
+  ________.__  __                   .__  .__ 
+ /  _____/|__|/  |_            ____ |  | |__|
+/   \  ___|  \   __\  ______ _/ ___\|  | |  |
+\    \_\  \  ||  |   /_____/ \  \___|  |_|  |
+ \______  /__||__|            \___  >____/__|
+        \/                        \/         
+`
+
 func main() {
-	
+	fmt.Print(logo.Wrap(gitlogo))
 }
